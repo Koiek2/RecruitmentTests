a4e4abfd7d289a46f133b700015defde6bd3404a
Koiek2 <koi2@wp.pl>
2017-10-15 16:08:24 +0200

    init project

---
+++ src/main/java/com/ampm/blue/RecruitmentTests/RecruitmentTestsApplication.java
@@ -0,0 +1,12 @@
+package com.ampm.blue.RecruitmentTests;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecruitmentTestsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecruitmentTestsApplication.class, args);
+	}
+}

7477789569fb9aee052762123c8fc1d00a1d6370
Sergi Mascaró <sergi.mascaro@est.fib.upc.edu>
2017-12-23 16:33:38 +0100

    added language options
    - now the user is able to choose in which language he wants to search synonyms
    - if the word doesn't exist, exception is handled and the user is notified

--- src/main/resources/static/javascript/synonym_utilities.js
+++ src/main/resources/static/javascript/synonym_utilities.js
@@ -4,16 +4,23 @@ var WS_BASE = "http://thesaurus.altervista.org/thesaurus/v1";
 
 
 function lookForSynonym(word) {
+    lang = $("#lang")[0].value;
     $.ajax({
         url: WS_BASE,
         data: {
             word: word,
-            language: "en_US",
+            language: lang,
             output: "json",
             key: API_KEY
         },
         success: processResult,
         error: function (errorMessage) {
+            var results = document.getElementById("results");
+            var p = document.createElement("p");
+            var msg = document.createTextNode("The entered word might not exist. If it does, check your internet connection.");
+            p.style.color = "red";
+            p.appendChild(msg);
+            results.appendChild(p);
             console.log(errorMessage);
         }
     });
@@ -21,16 +28,23 @@ function lookForSynonym(word) {
 
 function processResult(data) {
     var list = document.createElement("ul");
-    var body = document.getElementsByTagName("body")[0];
+    var results = document.getElementById("results");
+    results.innerHTML = "";
     var array = data.response;
     var length = data.response.length;
     if (length > 3) length = 3;
     for (var i=0; i < length; i++) {
         var entry = document.createElement("li");
-        var node = document.createTextNode(array[i].list.synonyms);
+        var syns = array[i].list.synonyms
+            .split("|")
+            .slice(0, 3)
+            .join(", ");
+        var node;
+        if (syns.length > 3)
+        node = document.createTextNode(syns);
         entry.appendChild(node);
         list.appendChild(entry);
-        body.appendChild(list);
+        results.appendChild(list);
     }
 
 

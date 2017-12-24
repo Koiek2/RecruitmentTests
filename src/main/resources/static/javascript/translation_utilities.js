c2aed6f918a5f643417d151d73f9613c73fb69bb
Sergi Mascaró <sergi.mascaro@est.fib.upc.edu>
2017-12-24 19:48:34 +0100

    translation shown in test page
    - translation API call is executed correctly
    - errors handled in a proper way

--- src/main/resources/static/javascript/translation_utilities.js
+++ src/main/resources/static/javascript/translation_utilities.js
@@ -17,5 +17,20 @@ function translateText(text) {
 }
 
 function printTranslation(data) {
-    
+    var div = document.getElementById("translation");
+    div.innerHTML = "";
+    var p = document.createElement("p");
+    var txt = document.createTextNode(data["text"][0]);
+    p.appendChild(txt);
+    div.appendChild(p);
+}
+
+function handleError(errorMessage) {
+    var div = document.getElementById("translation");
+    var p = document.createElement("p");
+    var msg = document.createTextNode("The entered word might not exist. If it does, check your internet connection.");
+    p.style.color = "red";
+    p.appendChild(msg);
+    div.appendChild(p);
+    console.log(errorMessage);
 }
